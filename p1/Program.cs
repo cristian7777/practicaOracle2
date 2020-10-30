94b78804f6ca2f39e48e059b86e1b39606367d35
cristian7777 <56656815+cristian7777@users.noreply.github.com>
2020-10-29 22:41:53 -0600

    practica1

---
+++ p1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heeeey muy buenas a todos ");
+        }
+    }
+}
